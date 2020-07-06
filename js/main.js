977872556b8cd589dc206ee4c6750507e875d51d
tanphat2107 <apple@Bui-Lac.local>
2020-07-06 11:52:54 +0700

    tesst

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//     $('.slider').bxSlider();
+// });
+$(document).ready(function() {
+    $('#slick_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+      });
+});
